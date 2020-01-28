bf9166234212af60c377ac919045aed7b6e5b84d
hari.mopuri <hari.mopuri@gmail.com>
2020-01-28 10:32:17 +0000

    vpc subnet added

--- azure/resources/vpc/main.tf
+++ azure/resources/vpc/main.tf
@@ -9,3 +9,26 @@ resource "azurerm_virtual_network" "vn" {
     Terraform = "True"
   }
 }
+
+
+resource "azurerm_subnet" "pub_subnet" {
+  count                = length(var.public_subnets)
+  name                 = format(
+      "pub-%s-%d",
+      var.vn_name,
+      count.index) 
+  resource_group_name  = var.rg_name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  address_prefix       = element(var.public_subnets, count.index)
+}
+resource "azurerm_subnet" "private_subnet" {
+  count                = length(var.private_subnets)
+  name                 = format(
+      "priv-%s-%d",
+      var.vn_name,
+      count.index)
+  resource_group_name  = var.rg_name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  address_prefix       = element(var.private_subnets, count.index)
+
+}
--- azure/resources/vpc/variable.tf
+++ azure/resources/vpc/variable.tf
@@ -14,3 +14,13 @@ variable "rg_location" {
   type = string
   default = "West US"
 }
+
+#subnet required vairables
+variable "public_subnets" {
+   type =  list
+   default = [ "10.1.0.0/16" ] 
+}
+variable "private_subnets" {
+   type =  list
+   default = [ "10.2.0.0/16", "10.3.0.0/16", "10.4.0.0/16" ]
+}
